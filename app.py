9d67aa6b7e0bcb327bfe9107376df6d3f2003336
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-04-01 18:21:53 +0530

    updated flask app

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def index():
         csv_file = request.files['file']
         if not csv_file:
             return 'No file uploaded.', 400
+        if not field:
+            return 'No field selected.', 400
         df, dates, agent_ids, fields = read_csv_file(csv_file)
         df_filtered = data_manipulation(df, dates, agent_ids, fields, field.lower())
         plot = plot_graphs(df_filtered)
