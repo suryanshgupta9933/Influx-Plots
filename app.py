7b74591900c1a2c1490692105b12b321d0c067ae
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-04-01 18:02:26 +0530

    flask app

--- app.py
+++ app.py
@@ -5,17 +5,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    fields = ['Backup', 'Restore', 'Connection']
     if request.method == 'POST':
         field = request.form['field']
         csv_file = request.files['file']
         if not csv_file:
             return 'No file uploaded.', 400
         df, dates, agent_ids, fields = read_csv_file(csv_file)
-        df_filtered = data_manipulation(df, dates, agent_ids, fields, field)
+        df_filtered = data_manipulation(df, dates, agent_ids, fields, field.lower())
         plot = plot_graphs(df_filtered)
         return render_template('graph.html', image = plot)
     else:
-        return render_template('data.html')
+        return render_template('data.html', dropdown_options = fields)
 
 if __name__ == '__main__':
     app.run(debug=True)
